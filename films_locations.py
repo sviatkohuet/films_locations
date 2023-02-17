4fd5e6d3663332200b34fa18be25cb6494f17736
Sviatoslav <stehnii.pn@ucu.edu.ua>
2023-02-17 21:49:48 +0200

    added sort_by_distance

--- films_locations.py
+++ films_locations.py
@@ -8,7 +8,6 @@ import math
 def calculate_distance(locations):
     lat1, lon1 = locations[0]
     lat2, lon2 = locations[1]
-    # lon1, lat1, lon2, lat2 = map(math.radians, [lon1, lat1, lon2, lat2])
 
     radius = 6371  # km
  
@@ -45,35 +44,53 @@ def get_coordinates(address):
 
     try:
         response = requests.get(url).json()[0]
-        print([float(response['lat']), float(response['lon'])])
         return [float(response['lat']), float(response['lon'])]
     except (IndexError, json.decoder.JSONDecodeError):
         return [0, 0]
 
 
-# def sort_by_distance(locations):
-#     pass
+def sort_by_distance(locations):
+    sorted_locations = {}
+    for film in locations.keys():
+        sorted_locations[film] = []
+        for location in locations[film]:
+            coordinates = get_coordinates(location)
+            if coordinates != [0, 0]:
+                sorted_locations[film].append([calculate_distance([[34.0536909, -118.242766], coordinates]), location])
+        sorted_locations[film] = sorted(sorted_locations[film], key=lambda x: x[0])
+    return sorted_locations
 
-def generate_map(start_location, file):
+def generate_map(start_location, file, radius=None):
     locations = get_locations(file, start_location)
-    # sorted_locations = sort_by_distance(locations)
+    sorted_locations = sort_by_distance(locations)
     map = folium.Map(tiles="Stamen Terrain",
                 location=start_location,
                 zoom_control=3)
     fg = folium.FeatureGroup(name="Films map")
     number = 0
+    if radius==None:
+        for film in sorted_locations.keys():
+            if number >= 20:
+                break
+            for location in sorted_locations[film]:
+                coordinates = get_coordinates(location[1])
+                if coordinates != [0, 0]:
+                    fg.add_child(folium.Marker(location=[coordinates[0], coordinates[1]],
+                                        popup=film, icon=folium.Icon()))                    
+                    number += 1
+        return 1
     for _, element in enumerate(locations.keys()):
-        if number >= 20:
+        if number >= 10:
             break
         for location in locations[element]:
             coordinates = get_coordinates(location)
-            if coordinates != [0, 0] and calculate_distance([start_location, coordinates]) <= 2000:
+            if coordinates != [0, 0] and calculate_distance([start_location, coordinates]) <= radius:
                 fg.add_child(folium.Marker(location=[coordinates[0], coordinates[1]],
                                     popup=element,
                                     icon=folium.Icon()))
                 number += 1
     map.add_child(fg)
     map.save("Map.html")
+    return 1
 
-
-generate_map([34.0536909, -118.242766], 'locations.list')
+# generate_map([34.0536909, -118.242766], 'locations.list', 3000)
